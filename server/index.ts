bc568107cd2d276784edf56d9e75036913a24f2e
muhammadAbubakarbilal <abgakhar787@mail.com>
2025-07-01 12:27:47 +0500

    first commit

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-
+// just a commit
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
